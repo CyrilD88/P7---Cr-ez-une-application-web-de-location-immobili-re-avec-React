622d5fd9780f50e51c106ddb1caa23312a22d567
Cyril Derc <cyril_derc@hotmail.com>
2023-04-14 19:55:17 +0200

    création de la page 404

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { Link } from 'react-router-dom'
+import '../styles/Error.css'
 
-function Error(){
-    return (
-        <div>
-            Test error Works !
-        </div>
-    )
-};
-
-export default Error;
+export default function Error() {
+  return (
+    <div className="error">
+      <h1 className="error-404">404</h1>
+      <span className="errorDescription">
+        Oups! La page que vous demandez n'existe pas.
+      </span>
+      <Link to="/" className="errorReturn">
+        Retourner sur la page d’accueil
+      </Link>
+    </div>
+  )
+}
