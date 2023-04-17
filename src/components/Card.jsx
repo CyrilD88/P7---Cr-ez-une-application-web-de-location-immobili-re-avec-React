89b37ff0a5474cc0e77caf6cbc9474a9146c9ae0
Cyril Derc <cyril_derc@hotmail.com>
2023-04-17 15:38:24 +0200

    création des fiche appart sur la page d'accueil

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,39 +1,30 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import AppartementCard from '../assets/Banner.png';
+
+import Data from "../Données.json";
 import "../styles/Card.css";
 
 function ApartmentCards() {
   return (
-    <div className="housing"> 
-     <Link to='Apartment'> 
-             <div className="housing__card">
-                 <img src={AppartementCard} alt="a mettre"/>
-                 <h3>Titre de la location</h3>
+   
+    <div className="logements">
+
+    {/* liste la base de données */}
+    {Data.map((apart) => {
+        const { id, cover, title } = apart;
+
+        // affiche la fiche logement sur la page d'accueil
+        return (
+            <div className="fiche-logement" key={id}>
+                <Link to={{ pathname: "Apartment", search: "?_id="+id }}>
+                    <img src={cover} alt={title} />
+                    <h3>{title}</h3>
+                </Link>
             </div>
-     </Link>
-        <div className="housing__card">
-        <img src={AppartementCard} alt="a mettre"/>
-            <h3>Titre de la location</h3>
-        </div>
-        <div className="housing__card">
-        <img src={AppartementCard} alt="a mettre"/>
-            <h3>Titre de la location</h3>
-        </div>
-        <div className="housing__card">
-        <img src={AppartementCard} alt="a mettre"/>
-            <h3>Titre de la location</h3>
-        </div>
-        <div className="housing__card">
-        <img src={AppartementCard} alt="a mettre"/>
-            <h3>Titre de la location</h3>
-        </div>
-        <div className="housing__card">
-        <img src={AppartementCard} alt="a mettre"/>
-            <h3>Titre de la location</h3>
-        </div>
-     
+        )
+    })}
     </div>
+
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-
 import Banner from '../components/Banner'
-
-import ApartmentCards from '../components/Card'
+import ApartmentCards from '../components/Card' 
+// import Data from '../Données.json'
 
 import "../styles/Home.css"
 
 function Home() {
     return (
-        <div role="main">
+        <div >
                
                 <Banner>
                     <div className="banner banner-commun">
                         <h1>Chez vous, partout et ailleurs</h1>
                     </div>
                 </Banner>
-                <ApartmentCards />
+                <ApartmentCards/>
+               {/* <section className='card-container'>
+                {Data.map((apart)=>(
+                    <ApartmentCards key={apart.id} data={apart}/>
+                ))}
+               </section>  */}
           
         </div>
     )
